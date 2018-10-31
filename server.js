e3b133233de548ca0b8427ecc39ea702e0a1d5c6
Tanner Goshert <tgosh206@hotmail.com>
2018-10-31 16:02:25 -0700

    added runServer and closeServer

--- server.js
+++ server.js
@@ -11,6 +11,45 @@ app.use(express.json());
 // requests to HTTP requests to `/blog-posts` to `blogPostsRouter`
 app.use("/blog-posts", blogPostsRouter);
 
-app.listen(process.env.PORT || 8080, () => {
-  console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
-});
+// runServer and closeServer need access to the server object,
+// so we declare 'server' here and when runServer runs, a value
+// gets assigned to it
+let server;
+
+// starts the server and returns a promise.
+function runServer() {
+  const port = process.env.PORT || 8080;
+  return new Promise((resolve, reject) => {
+    server = app
+      .listen(port, () => {
+        console.log(`Your app is listening on port ${port}`);
+        resolve(server);
+      })
+      .on("error", err => {
+        reject(err);
+      });
+  });
+}
+
+// like `runServer`, this function also returns a promise.
+// `server.close` doesn't return a promise on its own, so we
+// create one ourselves.
+function closeServer() {
+  return new Promise((resolve, reject) => {
+    console.log("Closing server");
+    server.close(err => {
+      if (err) {
+        reject(err);
+        // so we don't also call `resolve()`
+        return;
+      }
+      resolve();
+    });
+  });
+}
+
+if (require.main === module) {
+  runServer().catch(err => console.error(err));
+}
+
+module.exports = { app, runServer, closeServer };
